46ce06d57d3dfe480d01246253e5d3c685162fde
chris482 <christrinh5@gmail.com>
2019-09-14 17:51:21 -0700

    Add validating moves and make a move methods

--- W4D2/Mancala/lib/board.rb
+++ W4D2/Mancala/lib/board.rb
@@ -19,9 +19,32 @@ class Board
   end
 
   def valid_move?(start_pos)
+    raise 'Invalid starting cup' if !cups.include?(start_pos)
+    raise "Starting cup is empty" if @cups[start_pos].empty?  
   end
 
   def make_move(start_pos, current_player_name)
+    # empties cup
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+
+    # distributes stones
+    cup_idx = start_pos
+    until stones.empty?
+      cup_idx += 1
+      cup_idx = 0 if cup_idx > 13
+      # places stones in the correct current player's cups
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif cup_idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[cup_idx] << stones.pop
+      end
+    end
+
+    render
+    next_turn(cup_idx)
   end
 
   def next_turn(ending_cup_idx)
