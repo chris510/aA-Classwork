0ecef7c0c478e78ced8763d54bc9fcdd36dfc79f
chris482 <christrinh5@gmail.com>
2019-09-22 22:10:08 -0700

    Add query methods

--- W6D1/Movie-buff/movie_novice/queries.rb
+++ W6D1/Movie-buff/movie_novice/queries.rb
@@ -23,18 +23,21 @@
 
 def find_angelina
   #find Angelina Jolie by name in the actors table
+  Actor.find_by(name: 'Angelina Jolie')
 
 end
 
 def top_titles
   # get movie titles from movies with scores greater than or equal to 9
   # hint: use 'select' and 'where'
-
+  .select(:id, :title)
+  .where('score >= 9')
 end
 
 def star_wars
   #display the id, title and year of each Star Wars movie in movies.
   # hint: use 'select' and 'where'
+  .select(:id, :title, :yr)
 
 end
 
