3da76e2bbf2c72725a7ae08496fceba39a05de51
chris482 <christrinh5@gmail.com>
2019-09-29 21:41:07 -0700

    Add change password method

--- W7D1/rails_auth/app/models/user.rb
+++ W7D1/rails_auth/app/models/user.rb
@@ -27,5 +27,9 @@ class User < ApplicationRecord
     self.session_token ||= User.generate_session_token
   end
 
-  def
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
 end
