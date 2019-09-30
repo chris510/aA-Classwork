63ce558d78053c2c380ec3f2f54f73bf80ab83aa
chris482 <christrinh5@gmail.com>
2019-09-29 21:40:12 -0700

    add login-session tokens

--- W7D1/rails_auth/app/models/user.rb
+++ W7D1/rails_auth/app/models/user.rb
@@ -5,4 +5,27 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6, allow_nil : true }
   before_validation :ensure_session_token
   
+  #check if given password is the same regarding checksum and salt
+  def self.find_by_crendentials(username, password) 
+    user = User.find_by(username: username)
+    return user if user == true && BCrypt::Password.new(user.password_digest).is_password?(password)
+    nil
+  end
+
+  def self.generate_session_token
+
+  end
+
+  def reset_session_token
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  #sees if a token is always set
+  def ensure_session_token 
+    self.session_token ||= User.generate_session_token
+  end
+
+  def
 end
