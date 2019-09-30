81defaaf32cb9b32c0e679ac6e798466bbb803cc
chris482 <christrinh5@gmail.com>
2019-09-29 21:35:20 -0700

    Add user model constraints

--- W7D1/rails_auth/app/models/user.rb
+++ W7D1/rails_auth/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :username, presence: true
+  validates :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: { message: 'Password can\t be blank' }
+  validates :password, length: { minimum: 6, allow_nil : true }
+  before_validation :ensure_session_token
+  
 end
