44cb7ae7c817cda20a01390e3e87aad73a9e97e1
chris482 <christrinh5@gmail.com>
2019-10-11 08:59:22 -0700

    Add reader function

--- W8D5/callback_function_calls_ex/asynchronous_func.js
+++ W8D5/callback_function_calls_ex/asynchronous_func.js
@@ -8,4 +8,48 @@ function hammerTime(time) {
   });
 }
 
-console.log('Do you like tea?')
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function teaAndBiscuits() {
+  reader.question('Would you like some tea?', function (res) {
+    console.log(`You replied ${res}.`);
+    reader.question('Would you like some biscuits?', function (res2) {
+      console.log(`You replied ${res2}.`);
+
+      const first = (res === 'yes') ? 'do' : 'don\'t';
+      const second = (res2 === 'yes') ? 'do' : 'don\'t';
+
+      console.log(`So you ${first} want tea and you ${second} want biscuits.`);
+      reader.close();
+    });
+  });
+}
+
+function Cat() {
+  this.name = 'Markov';
+  this.age = 3;
+}
+
+function Dog() {
+  this.name = 'Noodles';
+  this.age = 4;
+}
+
+Dog.prototype.chase = function (cat) {
+  console.log(`My name is ${this.name} and I'm chasing ${cat.name}! Woof!`)
+};
+
+const Markov = new Cat();
+const Noodles = new Dog();
+
+// Method style
+
+Noodles.chase(Markov);
+
+Noodles.chase.call(Markov, Noodles);
+Noodles.chase.apply(Markov, [Noodles]);
