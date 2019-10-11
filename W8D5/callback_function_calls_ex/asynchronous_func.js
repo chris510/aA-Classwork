eb57034e536ba3286265ec07dca598c93c877a40
chris482 <christrinh5@gmail.com>
2019-10-11 08:45:59 -0700

    Add windowtimeout function

---
+++ W8D5/callback_function_calls_ex/asynchronous_func.js
@@ -0,0 +1,11 @@
+window.setTimeout(function (){
+  alert('Hammer Time!');
+}, 5000);
+
+function hammerTime(time) {
+  window.setTimeout(function () {
+    alert(`${time} is hammertime!`)
+  });
+}
+
+console.log('Do you like tea?')
