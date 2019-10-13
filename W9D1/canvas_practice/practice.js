5264be13246baa8f16bdc8738ebba94893ed182e
chris482 <christrinh5@gmail.com>
2019-10-13 16:17:04 -0700

    create circle and rec in canvas

--- W9D1/canvas_practice/practice.js
+++ W9D1/canvas_practice/practice.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function(){
+  const canvas = document.getElementById('mycanvas');
+  canvas.height = 500;
+  canvas.width = 500;
 
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = "blue";
+  ctx.fillRect(20, 10, 150, 100);
+
+  ctx.beginPath();
+  ctx.arc(100, 100, 20, 0, 2 * Math.PI, true);
+  ctx.strokeStyle = "red";
+  ctx.lineWidth = 5;
+  ctx.stroke();
+  ctx.fillStyle = "orange";
+  ctx.fill();
 });
